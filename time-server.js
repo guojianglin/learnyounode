76073cd9c89d2f3ca4b3e34940c38fafdbbcb346
guojianglin <1448373124@qq.com>
2020-01-19 18:27:15 +0800

    time server fixed

--- time-server.js
+++ time-server.js
@@ -1,5 +1,6 @@
 const net = require('net');
-const port = process.argv[3];
+const port = process.argv[process.argv.length - 1];
+console.log(process.argv);
 const server = net.createServer((socket) => {
     const current = new Date();
     const year = current.getFullYear();
@@ -7,7 +8,7 @@ const server = net.createServer((socket) => {
     const day = current.getDate();
     const hour = current.getHours();
     const min = current.getMinutes();
-    const format = `${year}-${ month > 9 ? '' : 0}${ month}-${ day > 9 ? '' : 0}${day} ${ hour > 9 ? '' : 0}${hour}:${ day > 9 ? '' : 0}${min}`;
+    const format = `${year}-${ month > 9 ? '' : 0}${ month}-${ day > 9 ? '' : 0}${day} ${ hour > 9 ? '' : 0}${hour}:${ day > 9 ? '' : 0}${min}\n`;
     // console.log(format)
     // socket.end(format);
     socket.write(format);
